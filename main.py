695154bbd22b22b002c1c1b675f0a04869a18996
espona <lucia.espona@gmail.com>
2024-11-14 17:20:55 +0100

    Basic commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+# Gather our code in a main() function
+def main():
+    print("Hello world!")
+
+
+# Standard boilerplate to call the main() function to begin
+# the program.
+if __name__ == '__main__':
+    main()
